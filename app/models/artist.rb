6260597847ec5bcf2eead91bf1843915c0af4c41
Matthew Dias Prada <matgems@googlemail.com>
2019-08-31 21:32:34 +0100

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,44 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map {|painting| painting.gallery}
+  end
+
+  def cities
+    galleries.map {|gallery| gallery.city}.uniq
+  end
+
+  def paintings_per_year
+    paintings.length / @years_experience
+  end
+
+  def create_painting(artist, title, price)
+    Painting.new(self, artist, title, price)
+  end
+
+  def self.total_experience
+    @@all.sum {|artist| artist.years_experience}
+  end
+
+  def self.most_prolific
+    @@all.max_by {|artist| artist.paintings_per_year}
+  end
+
+  def self.all
+    @@all
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,22 @@
 require_relative '../config/environment.rb'
 
+a1 = Artist.new("Picasso", 2)
+a2 = Artist.new("Monet", 20)
+a3 = Artist.new("Ross", 40)
+a4 = Artist.new("Mike", 7)
 
+g1 = Gallery.new("Fancy", "New York")
+g2 = Gallery.new("Classy", "New Orleans")
+g3 = Gallery.new("The Louvre", "Paris")
+g4 = Gallery.new("Night Gallery", "Unknown")
+
+
+Painting.new(a1, g1, "Flowers", 200)
+Painting.new(a1, g2, "Book", 300)
+Painting.new(a2, g3, "Ducks", 10000)
+Painting.new(a2, g4, "Boat", 1000)
+a3.create_painting(g3, "Fish", 1)
+a4.create_painting(g2, "Two dogs on a boat", 55)
 
 
 binding.pry
