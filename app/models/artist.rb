518caa837509b0b0238d060be4e65c5e53ff57fd
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-09-03 10:46:34 +0100

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,50 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+ 
+  #Returns an array of all the artists
+  def self.all
+    @@all
+  end
+
+  #Given the arguments of title, price and gallery, creates a new painting belonging to that artist
+  def create_painting(title, price, gallery)
+    painting = Painting.new(title, price)
+    painting.artist = self
+    painting.gallery = gallery
+    painting
+  end
+
+  #Returns an array all the paintings by an artist
+  def paintings
+    Painting.all.select{ |painting| painting.artist == self }
+  end
+
+  #Returns an array of all the galleries that an artist has paintings in
+  def galleries
+    self.paintings.map{ |painting| painting.gallery }
+  end
+
+  #Return an array of all cities that an artist has paintings in
+  def cities
+    self.galleries.map{ |gallery| gallery.city }
+  end
+
+  #Returns an integer that is the total years of experience of all artists
+  def self.total_experience
+    @@all.sum{ |artist| artist.years_experience }
+  end
+
+  #Returns an instance of the artist with the highest amount of paintings per year of experience.
+  def self.most_prolific
+    @@all.max_by{ |artist| artist.paintings.length / artist.years_experience }
   end
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,27 @@ require 'pry'
 require_relative '../app/models/artist.rb'
 require_relative '../app/models/gallery.rb'
 require_relative '../app/models/painting.rb'
+
+#SEED DATA
+
+a1 = Artist.new("John", 12)
+a2 = Artist.new("Sarah", 30)
+a3 = Artist.new("Tom", 5)
+
+g1 = Gallery.new("The MET", "New York")
+g2 = Gallery.new("Tate Liverpool", "Liverpool")
+g3 = Gallery.new("British Museum", "London")
+g4 = Gallery.new("Tate Modern", "London")
+
+p1 = Painting.new("The Waterfall", 1200)
+p2 = Painting.new("Time", 900)
+p3 = Painting.new("Earth and Space", 500)
+
+a1.create_painting("Rainbows", 3000, g1)
+a2.create_painting("Blue", 200, g2)
+a3.create_painting("Red", 20, g3)
+a1.create_painting("Colours", 400, g4)
+a2.create_painting("Rainbows", 3000, g3)
+
+binding.pry
+'save'
